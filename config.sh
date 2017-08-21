b9ac5936ff1c501ce564bd5470fba5f8303c96d0
volcanoh <huang.volcano@gmail.com>
2017-08-21 19:41:33 +0800

    update config and sync

---
+++ config.sh
@@ -0,0 +1,6 @@
+cp -r vim/* ~/.vim_rumtime/
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+cp -r oh-my-zsh ~/.oh-my-zsh
+source ~/.oh-my-zsh/.zshrc
+cp flake8 ~/.config/flake8
+
---
+++ sync.sh
@@ -0,0 +1,4 @@
+cp -r ~/.vim_rumtime/* vim/*
+cp -r ~/.oh-my-zsh/* oh-my-sh/*
+cp ~/.config/flake8 flake8
+
