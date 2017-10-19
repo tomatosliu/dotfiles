ea10573eaf5a55e5f4a1f4e15fc4f6d3c4b040c0
Siyuan Liu <siyuan.liu@tusimple.ai>
2017-10-19 12:32:42 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,6 +5,9 @@ set -e
 # Require Vim with Lua
 
 sudo pip install flake8
+sudo mkdir -p ~/.vim_runtime
+sudo mkdir -p ~/.oh-my-zsh
+sudo mkdir -p ~/.config
 cp -R vim/* ~/.vim_runtime/
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp -R oh-my-zsh/* ~/.oh-my-zsh/
