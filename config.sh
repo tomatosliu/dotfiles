92e36cbd3e6748df76379868c17d8cd642cac671
Siyuan Liu <siyuan.liu@tusimple.ai>
2017-10-19 12:51:20 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -12,6 +12,7 @@ cp -R vim/* ~/.vim_runtime/
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp -R oh-my-zsh/* ~/.oh-my-zsh/
 chsh -s /bin/zsh
-cp ~/.oh-my-zsh/.zshrc ~/
+cp oh-my-zsh/.zshrc ~/
 cp flake8 ~/.config/flake8
-
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
